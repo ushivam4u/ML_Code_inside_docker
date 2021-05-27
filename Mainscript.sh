5287c71deb18a7c465100ec4c0ac815e0daed7fa
Shivam Prasad Upadhyay <67913868+ushivam4u@users.noreply.github.com>
2021-05-27 11:23:50 +0530

    Update Mainscript.sh

--- Mainscript.sh
+++ Mainscript.sh
@@ -1,4 +1,4 @@
 docker pull centos:latest
 docker run -dit --name summertask1 centos:latest
 docker cp /root/SalaryData.csv summertask1:/
-docker cp /root/summertaskcode summertask1:/
+docker cp /root/summer_task1_code summertask1:/
