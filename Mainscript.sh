0cdae1001ff98feb3e3c0d46f78e692e32fd2a84
Shivam Prasad Upadhyay <67913868+ushivam4u@users.noreply.github.com>
2021-05-27 16:52:32 +0530

    Update Mainscript.sh

--- Mainscript.sh
+++ Mainscript.sh
@@ -3,3 +3,4 @@ docker run -dit --name summertask1 centos:latest
 docker cp /root/SalaryData.csv summertask1:/
 docker cp /root/summer_task1_code summertask1:/
 docker cp /root/python_install_inside_docker.sh summertask1:/
+docker exec -it summertask1 bash python_install_inside_docker.sh
